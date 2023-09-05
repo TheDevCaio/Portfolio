d05ce4bbba5c167d90bca76117bafc4939d84a22
Caio Dev <110746976+TheDevCaio@users.noreply.github.com>
2023-09-05 17:36:34 -0300

    Creating class MyContact.js

---
+++ portfolio/src/components/contact/MyContact.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class MyContact extends Component {
+  render() {
+    return (
+      <>
+        <Projects page="contact" />
+      </>
+    );
+  };
+};
