1f9ebceb51d55d63c59b055442ed8d0014fd08f6
Caio Dev <110746976+TheDevCaio@users.noreply.github.com>
2023-09-05 17:50:22 -0300

    Creating routes and structuring archives.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/navBar/NavBar';
+import BackgroundMain from './components/background/BackgroundMain';
+import Info from './components/info/Info';
+import Abilitys from './components/abilitys/Abilitys';
+import GameIndex from './components/gameIndex/GameIndex';
+import Projects from './components/projects/ProjectsM';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* Rota principal */}
+        <Route
+          path="/"
+          element={
+            <>
+              <NavBar />
+              <BackgroundMain />
+              <Info />
+              <GameIndex /> {/* Renderiza o GameIndex apenas quando a rota é / */}
+            </>
+          }
+        />
+        {/* Rota de habilidades */}
+        <Route path="/skills" element={<><><Abilitys /><BackgroundMain /></><NavBar /></>} />
+
+        <Route
+          path="projects"
+          element={
+            <>
+              <NavBar className="nav-projects-fixed" /> {/* Adicione a classe nav-projects-fixed aqui */}
+              <Projects /> 
+              <BackgroundMain />
+            </>
+          }
+        />
+
+        <Route
+          path="projects/contact"
+          element={
+            <>
+              <NavBar /> 
+              <BackgroundMain />
+            </>
+          }
+        />
+
+        <Route
+          path="contact"
+          element={
+            <>
+              <NavBar /> 
+              <BackgroundMain />
+            </>
+          }
+        />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
