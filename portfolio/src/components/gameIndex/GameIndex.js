efe342eef456d355cc64520f6bb3cd13657f061a
Caio Dev <110746976+TheDevCaio@users.noreply.github.com>
2023-09-05 17:37:31 -0300

    Creating GameIndex.js

---
+++ portfolio/src/components/gameIndex/GameIndex.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/gameIndex.css";
+
+const GameIndex = () => {
+  return (
+    <div id="Index">
+      <h1>CAIO GIRON</h1>
+      <div id="Level_Lines">
+        <span id="Level"></span>
+        <span id="Level_Shadow"></span>
+      </div>
+      <h2>Fullstack Developer</h2>
+    </div>
+  );
+};
+
+export default GameIndex;
